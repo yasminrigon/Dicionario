f7b469aee791cf2c19103d28beb5f2b707597ccc
yasminrigon <137406871+yasminrigon@users.noreply.github.com>
2023-12-07 09:13:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,100 @@
+document.getElementById('div').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('beach');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('div2').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('exchange');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('div3').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('girl');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('div4').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('strawberry');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('div5').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('summer');
+
+    if (minhaDiv.style.display === 'none') {
+    minhaDiv.style.display = 'block';
+    } 
+    else {
+    minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('div6').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('sunshine');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('div7').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('swimming');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('div8').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('thirty');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+    });
+document.getElementById('div9').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('water');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
+document.getElementById('div10').addEventListener('click', function() {
+    let minhaDiv = document.getElementById('window');
+
+    if (minhaDiv.style.display === 'none') {
+        minhaDiv.style.display = 'block';
+    } 
+    else {
+        minhaDiv.style.display = 'none';
+    }
+});
